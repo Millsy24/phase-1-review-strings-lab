1155ee68b942fc5b70d1be6c73158b4f8262db7c
Lukas Mills <lukascodes64@gmail.com>
2022-06-27 17:46:02 -0400

    commit tree afcfbb0c5a7d973fd06a7bf5c2e28f990527562f

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code in this file!
+ // Defining who the current user is
+const currentUser = 'Lukas';
+const string ='Welcome to Flatbook,';
+
+
+
+// Setting a variable to our welcome message
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Setting a variable to have an excited welcome message!
+const upperCaseString = string.toUpperCase();
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const excitedWelcomeMessage = `${upperCaseString} ${upperCaseCurrentUser}!`;
+
+// Setting a variable to have a shortGreeting
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
